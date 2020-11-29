500f6cc1191c1637ed48bfaaa5482386b4b602f2
Jose Perera <70457016+josevictorperera@users.noreply.github.com>
2020-11-29 17:51:45 -0600

    Update script.js

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -116,7 +116,7 @@ var chosenCategoryShortName =chooseRandomCategory(categories);
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "{{randomCategoryShortName}}","'" + chosenCategoryShortName + "'" );
+var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "{{randomCategoryShortName}}","'" + chosenCategoryShortName + "'");
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
