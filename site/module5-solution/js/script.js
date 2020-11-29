a8ff5559a21f31c056f49f024c0329539012c17a
jose <jose@localhost.localdomain>
2020-11-29 16:27:00 -0600

    My Fifth page.

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -102,8 +102,8 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
+var chosenCategoryShortName =chooseRandomCategory(categories)
 
-         var chosenCategoryShortName = chooseRandomCategory(categories);
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -116,14 +116,13 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-
-         var homeHtmlToInsertIntoMainPage = insertProperty(html, "randomCategoryShortName", "'"+chosenCategoryShortName+"'");
+var homeHtmlToInsertIntoMainPage = insertProperty(html, "{{randomCategoryShortName}}","'" + chosenCategoryShortName + "'" )
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
